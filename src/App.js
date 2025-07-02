0569e6d63f489d23fee627f279836dd288a9e135
Ross <adam.ross.phillips@gmail.com>
2025-07-02 22:01:55 +0100

    UI Enhancements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Home from './components/Home';
 import GameList from './GameList';
+// import GameForm from './GameForm'; // Uncomment when this exists
+// import GameDetail from './GameDetail'; // Uncomment when this exists
 
 function App() {
   return (
-    <div>
-      <GameList />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/games" element={<GameList />} />
+        {/* Add more routes as you build them */}
+        {/* <Route path="/new-game" element={<GameForm />} /> */}
+        {/* <Route path="/games/:id" element={<GameDetail />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
