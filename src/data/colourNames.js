60edd7085a398b4b25f434a52f4ee550d5c71324
Ross <adam.ross.phillips@gmail.com>
2025-07-13 17:31:17 +0100

    final test

---
+++ src/data/colourNames.js
@@ -0,0 +1,13 @@
+const colorNames = {
+  "#ff3333": "Red",
+  "#008000": "Green",
+  "#3333ff": "Blue",
+  "#000000": "Black",
+  "#ffff00": "Yellow",
+  "#ffa500": "Orange",
+  "#b300b3": "Purple",
+  "#ff00ff": "Magenta",
+  "#ffffff": "White", // just in case
+};
+
+export default colorNames;
